fa365a5f7299492ad36dd366cbab2d376e58e6b1
Essence <esskennedy2@gmail.com>
2019-03-23 23:47:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrase Tyson"
 
 var greetSpecialGuest = `Hello," + ${specialGuest} + !`
 
-var  topic = "space";
+var  topic = "space"
 
 var conversation = "Let's talk about" + ${topic};
